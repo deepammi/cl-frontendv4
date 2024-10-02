034b170985aa27c67b66d92078c5c856ac37bc9e
Kapil <bishnoikapil0@gmail.com>
2024-10-02 11:03:35 +0530

    Login logout functionality is done in navbar.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,19 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Layout, Button, Drawer, Menu, Flex, Typography } from "antd";
+import { Layout, Button, Drawer, Menu, Typography } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
 import Image from "next/image";
 import logoIconUrl from "@Image/CompanyLogo.svg";
 import { navMenu } from "@/config/NavMenu";
 import { useRouter } from "next/navigation";
+import useAuth from "../useAuth";
+
 const Navbar = () => {
+  const isLoggedIn = useAuth();
   const router = useRouter();
   const [visible, setVisible] = useState(false);
   const [selectedMenu, setSelectedMenu] = useState<string[]>(["pre_sales_ai"]);
+
   const showDrawer = () => {
     setVisible(!visible);
   };
@@ -30,60 +34,37 @@ const Navbar = () => {
     navigateToHandler(e.key);
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    router.push("/login");
+  };
+
   return (
-        <Layout.Header className="absolute top-5" style={{ backgroundColor: "transparent", width:"100%" }}>
-          <Flex justify={"space-between"} align={"center"}>
-            <Flex gap={4} align="center">
-              <Image src={logoIconUrl} alt="logo" width={60} height={60} />
-              <Title level={3} style={{ color: "#767BDE", fontWeight: "800" }}>
-                Turigma.ai
-              </Title>
-            </Flex>
-            <div className="hidden md:block flex" style={{ minWidth: "50%" }}>
-              <Flex align="center" gap={5}>
-                <Menu
-                  onClick={handleNavMenuClick}
-                  selectedKeys={selectedMenu}
-                  mode="horizontal"
-                  items={navMenu}
-                  style={{
-                    borderBottom: "none",
-                    minWidth: "90%",
-                    backgroundColor:"transparent",
-                  }}
-                />
-                <Button
-                  type="primary"
-                  shape="round"
-                  onClick={() => navigateToHandler("sign_in")}
-                >
-                  Sign in
-                </Button>
-              </Flex>
-            </div>
-            <div className="block md:hidden">
-              <Button
-                className="block md:hidden"
-                type="text"
-                onClick={showDrawer}
-              >
-                <MenuOutlined />
-              </Button>
-            </div>
-            <Drawer
-              title={"Turigma.ai"}
-              placement="right"
-              closable={true}
-              onClose={showDrawer}
-              open={visible}
-              style={{ zIndex: 99999, color: "#767BDE" }}
-            >
-              <Menu
-                onClick={handleNavMenuClick}
-                selectedKeys={selectedMenu}
-                mode="vertical"
-                items={navMenu}
-              />
+    <Layout.Header
+      className="absolute top-5"
+      style={{ backgroundColor: "transparent", width: "100%" }}
+    >
+      <div className="flex justify-between items-center">
+        <div className="flex gap-4 items-center">
+          <Image src={logoIconUrl} alt="logo" width={60} height={60} />
+          <Title level={3} style={{ color: "#767BDE", fontWeight: "800" }}>
+            Turigma.ai
+          </Title>
+        </div>
+        <div className="hidden md:flex" style={{ minWidth: "50%" }}>
+          <div className="flex items-center gap-5">
+            <Menu
+              onClick={handleNavMenuClick}
+              selectedKeys={selectedMenu}
+              mode="horizontal"
+              items={navMenu}
+              style={{
+                borderBottom: "none",
+                minWidth: "90%",
+                backgroundColor: "transparent",
+              }}
+            />
+            {!isLoggedIn ? (
               <Button
                 type="primary"
                 shape="round"
@@ -91,9 +72,48 @@ const Navbar = () => {
               >
                 Sign in
               </Button>
-            </Drawer>
-          </Flex>
-        </Layout.Header>
+            ) : (
+              <Button type="primary" shape="round" onClick={handleLogout}>
+                Logout
+              </Button>
+            )}
+          </div>
+        </div>
+        <div className="block md:hidden">
+          <Button className="block md:hidden" type="text" onClick={showDrawer}>
+            <MenuOutlined />
+          </Button>
+        </div>
+        <Drawer
+          title={"Turigma.ai"}
+          placement="right"
+          closable={true}
+          onClose={showDrawer}
+          open={visible}
+          style={{ zIndex: 99999, color: "#767BDE" }}
+        >
+          <Menu
+            onClick={handleNavMenuClick}
+            selectedKeys={selectedMenu}
+            mode="vertical"
+            items={navMenu}
+          />
+          {!isLoggedIn ? (
+            <Button
+              type="primary"
+              shape="round"
+              onClick={() => navigateToHandler("sign_in")}
+            >
+              Sign in
+            </Button>
+          ) : (
+            <Button type="primary" shape="round" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
+        </Drawer>
+      </div>
+    </Layout.Header>
   );
 };
 
