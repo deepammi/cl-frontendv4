c906e4c66d77537be330b7e3d0e85253e8a1c141
krishna kumar <krishna0512.kr@gmail.com>
2024-10-12 17:11:09 +0530

    added navbar to layout so that it will be accessible for all the pages.

--- src/app/benefit/page.tsx
+++ src/app/benefit/page.tsx
@@ -1,8 +1,9 @@
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 export default function page() {
   return (
-    <div className='flex flex-col items-center gap-4'>
+    <div className='mt-24 flex flex-col items-center gap-4'>
         <div className='text-2xl'>Benefit</div>
         <div>coming soon...</div>
     </div>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='flex flex-col items-center gap-4 mt-[2rem]'>
+    <div className='mt-24 flex flex-col items-center gap-4'>
         <div className='text-2xl'>My profile</div>
         <div>coming soon...</div>
     </div>
