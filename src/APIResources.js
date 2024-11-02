04d2ebb0767d49b4efe6609c1dda80ec6f10417d
deepammi <93964362+deepammi@users.noreply.github.com>
2024-11-02 14:31:55 -0400

    Update APIResources.js
    new backend

--- src/APIResources.js
+++ src/APIResources.js
@@ -4,6 +4,6 @@ const apiResources = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/" // Local backend URL
-      : "https://cl-backendv4-production.up.railway.app/", // Production backend URL
+      : "https://happy-mercy-staging.up.railway.app/", // Production backend URL old was cl-backendv4-production.up.railway.app/
 });
 export default apiResources;
