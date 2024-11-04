209f34aab72ea4627fd0af24d165741093a3ead0
deepammi <deepammishra@gmail.com>
2024-11-03 19:03:56 -0500

    Replaced hardcoded azure reference by env variable
    Earlier versions had hardcoded backend URL on azure. Changed that to a .env variable now

--- src/APIResources.js
+++ src/APIResources.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
+//const apiResources = axios.create({
+//    baseURL:
+//      process.env.NODE_ENV === "development"
+//        ? "http://localhost:4000/" // Local backend URL
+//        : "http://localhost:4000/", // Production backend URL old= https://cl-backendv4-production.up.railway.app/ new = happy-mercy-staging.up.railway.app/
+//  });
+  
 const apiResources = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:4000/" // Local backend URL
-      : "https://cl-backendv4-production.up.railway.app/", // Production backend URL
+      ? "https://happy-mercy-staging.up.railway.app/" // Local backend URL
+      : "https://happy-mercy-staging.up.railway.app/", // Production backend URL old= https://cl-backendv4-production.up.railway.app/ new = https://happy-mercy-staging.up.railway.app/
 });
+
 export default apiResources;
