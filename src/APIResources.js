cb9ae45f0b3c8056e3e92c8d49840d01c2309522
Saad Ahmed <saad@athenix.co>
2024-11-07 18:49:27 -0800

    added the correct railway endpoint for staging

--- src/APIResources.js
+++ src/APIResources.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const apiResources = axios.create({
+  // baseURL: "http://localhost:4000/"
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:4000/" // Local backend URL
+      ? "https://happy-mercy-staging.up.railway.app" // Local backend URL
       : "https://cl-backendv4-production.up.railway.app/", // Production backend URL
 });
 export default apiResources;
