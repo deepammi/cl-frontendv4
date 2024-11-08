a40948ef8468d752186a30c74e46371c83832d2e
Saad Ahmed <saad@athenix.co>
2024-11-08 14:34:37 -0800

    fixed railway

--- src/APIResources.js
+++ src/APIResources.js
@@ -1,18 +1,9 @@
 import axios from "axios";
 
-//const apiResources = axios.create({
-//    baseURL:
-//      process.env.NODE_ENV === "development"
-//        ? "http://localhost:4000/" // Local backend URL
-//        : "http://localhost:4000/", // Production backend URL old= https://cl-backendv4-production.up.railway.app/ new = happy-mercy-staging.up.railway.app/
-//  });
-  
 const apiResources = axios.create({
   // baseURL: "http://localhost:4000/" // uncomment and use this for LOCAL dev development. Make sure local server (cl-backendv4) is up and running
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "https://happy-mercy-staging.up.railway.app" // Staging backend URL
-      : "https://cl-backendv4-production.up.railway.app/", // Production backend URL
+  baseURL: "https://happy-mercy-staging.up.railway.app", // Staging backend URL
+  // baseURL : "https://cl-backendv4-production.up.railway.app/", // Replace with production backend URL for production
 });
 
 export default apiResources;
