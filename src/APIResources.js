66a2d5b37ce51941857f2a6c71913ff0d30d0e89
Saad Ahmed <saad@athenix.co>
2024-11-07 18:56:43 -0800

    minor cleanup

--- src/APIResources.js
+++ src/APIResources.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const apiResources = axios.create({
-  // baseURL: "http://localhost:4000/"
+  // baseURL: "http://localhost:4000/" // uncomment and use this for LOCAL dev development. Make sure local server (cl-backendv4) is up and running
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "https://happy-mercy-staging.up.railway.app" // Local backend URL
+      ? "https://happy-mercy-staging.up.railway.app" // Staging backend URL
       : "https://cl-backendv4-production.up.railway.app/", // Production backend URL
 });
 export default apiResources;
