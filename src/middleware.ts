d6e3e0961a2e7f822663511cd45f71109d14105a
Saad Ahmed <saad@athenix.co>
2024-11-08 12:00:44 -0800

    added more logging

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,6 +19,7 @@ export async function middleware(request: NextRequest, response: NextResponse) {
     },
   });
   console.log("responseAPI", responseAPI);
+  console.log("responseAPI.json()", await responseAPI.json());
 
   //Return to /login if token is not authorized
   if (responseAPI.status !== 200) {
